ea5e9c024ff26cd3dd6fa8ca04833be3cbfe668d
musa-devop <musa.consulting@outlook.com>
2021-11-15 22:05:14 +0100

    added test function many items

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from typing import List
 
 def total(xs: List[float]) -> float:
     """ Total returns the sum of xs"""
-    return -1.0 
+    result: float = 0.0
+    for i in xs:
+        result += i
+    return result
 
 
 
--- app_test.py
+++ app_test.py
@@ -8,3 +8,5 @@ def test_total_empty() -> None:
 def test_total_single_item() -> None:
     assert total([110.0]) == 110.0
     
+def test_total_many_items() -> None:
+    assert total([1.0, 2.0, 3.0]) == 6.0
